4a76677b416a9f1802993587f8ff4cdf0553c384
Harsh Choudhary <108018451+harsh-choudhary-nature@users.noreply.github.com>
2023-09-28 16:27:55 +0530

    Update index.js

--- harsh-testing-only/server/index.js
+++ harsh-testing-only/server/index.js
@@ -3,7 +3,17 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-
+app.get('/',(req,res)=> {
+  const filePath = path.join(__dirname, 'example.html'); // Change 'example.pdf' to the path of your PDF file.
+  fs.readFile(filePath,'utf8',(err,data)=>{
+    if(err){
+      res.status(500).send("Error reading html file")
+    }else{
+      res.setHeader('Content-Type','text/html');
+      res.send(data);
+    }
+  });  
+});
 app.get('/pdf', (req, res) => {
   const filePath = path.join(__dirname, 'example.pdf'); // Change 'example.pdf' to the path of your PDF file.
   const stat = fs.statSync(filePath);
@@ -14,6 +24,23 @@ app.get('/pdf', (req, res) => {
 
   const readStream = fs.createReadStream(filePath);
   readStream.pipe(res);
+});
+
+app.get('/pdf-download', (req, res) => {
+  const filePath = path.join(__dirname, 'example.pdf'); // Change 'example.pdf' to the path of your PDF file.
+  res.sendFile(filePath);
+  console.log("File is delivered from server side");
+});
+app.get('/downloads',(req,res)=> {
+  const filePath = path.join(__dirname, 'view-offline.html'); // Change 'example.pdf' to the path of your PDF file.
+  fs.readFile(filePath,'utf8',(err,data)=>{
+    if(err){
+      res.status(500).send("Error reading html file")
+    }else{
+      res.setHeader('Content-Type','text/html');
+      res.send(data);
+    }
+  });  
 });
 
 const PORT = process.env.PORT || 3000;
